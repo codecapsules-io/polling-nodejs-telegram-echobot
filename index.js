8be9620675cc45bf9847dc9f80dd44a2bc427c6b
AnesuMurakata <murac96@gmail.com>
2021-11-23 12:19:49 +0200

    Initial app commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Code Capsules telegram bot.\nI echo messages. Please try it', {
+    })
+})
+
+// copy every message and send to the user
+bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message))
+
+// Start bot via launch (polling) method 
+bot.launch()
+  
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
